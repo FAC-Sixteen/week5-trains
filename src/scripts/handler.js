86c50fb0a53721bd37541365ac0458c2c8e4772f
Samantha Dickeson <samlucydickeson@gmail.com>
2019-04-03 15:05:48 +0100

    get css working with server route

--- src/scripts/handler.js
+++ src/scripts/handler.js
@@ -1,21 +1,48 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const handleHomeRoute = (req, res) => {
-    const filePath = path.join(__dirname, '..', '..', 'public', 'index.html');
-    fs.readFile(filePath, (error, file) => {
-        if(error){
-            console.log(error);
-            res.writeHead(500, {'Content-Type': 'text/html'});
-            res.end('<h1>Sorry, problem on our end</h1>');
-            return
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(file);
-        }
-    })
-}
+  const filePath = path.join(__dirname, "..", "..", "public", "index.html");
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("<h1>Sorry, problem on our end</h1>");
+      return;
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(file);
+    }
+  });
+};
+
+const handlePublicRoute = (req, res) => {
+  const extension = req.url.split(".")[1];
+  const extensionType = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    jpg: "image/jpeg",
+    png: "image/png",
+    ico: "image/x-icon",
+    TTF: "font/ttf"
+  };
+
+  fs.readFile(path.join(__dirname, "..", "..", req.url), (error, file) => {
+    if (error) {
+      console.log(error);
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("<h1>500: server error</h1>");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": extensionType[extension]
+      });
+      res.end(file);
+    }
+  });
+};
 
 module.exports = {
-    handleHomeRoute
-}
+  handleHomeRoute,
+  handlePublicRoute
+};
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,14 +1,17 @@
-const handler = require('./handler')
+const handler = require("./handler");
 
 const router = (req, res) => {
-    if(req.url === '/'){
-        handler.handleHomeRoute(req, res)
-        // res.writeHead(200, {'content-type': 'text/html'})
-        // res.end('trains trains trains')
-    }else{
-        res.writeHead(404);
-        res.end('404 not found')
-    }
+  console.log(req.url);
+  if (req.url === "/") {
+    handler.handleHomeRoute(req, res);
+    // res.writeHead(200, {'content-type': 'text/html'})
+    // res.end('trains trains trains')
+  } else if (req.url.includes("/public/")) {
+    handler.handlePublicRoute(req, res);
+  } else {
+    res.writeHead(404);
+    res.end("404 not found");
+  }
 };
 
 module.exports = router;
