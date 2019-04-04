023846c686aadf175a1e687a425ae72a1fc4f0a1
Ania Blaziak <anblaziak@gmail.com>
2019-04-04 12:04:00 +0100

    remove localhost reference

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
-const hostname = process.env.HOSTNAME || 'localhost';
+// const hostname = process.env.HOSTNAME || 'localhost';
 const port = process.env.PORT || 5000;
 const router = require('./router');
 
-http.createServer(router).listen(port, hostname, () => {
-    console.log(`Server running at: http://${hostname}:${port}`)
+http.createServer(router).listen(port, () => {
+    console.log(`Server running at: http://localhost:${port}`)
 })
