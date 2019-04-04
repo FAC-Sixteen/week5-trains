47a3a6cd347e73917775948cd30353275f994a2e
Ania Blaziak <anblaziak@gmail.com>
2019-04-04 15:08:43 +0100

    fix clock to display leading zeros

--- public/clock.js
+++ public/clock.js
@@ -2,9 +2,13 @@ const clockDiv = document.querySelector('.clock');
 
 const getTime = () => {
     const currentTime = new Date();
-    const seconds = currentTime.getSeconds();
-    const minutes = currentTime.getMinutes();
-    const hours = currentTime.getHours();
+    let seconds = currentTime.getSeconds();
+    let minutes = currentTime.getMinutes();
+    let hours = currentTime.getHours();
+
+    seconds = seconds < 10 ? "0" + seconds : seconds
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    hours = hours < 10 ? "0" + hours : hours
 
     clockDiv.textContent = `${hours}:${minutes}:${seconds}`
 }
