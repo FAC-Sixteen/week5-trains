ad0a834428760f47863aca493f8b16bfab67bd23
Samantha Dickeson <samlucydickeson@gmail.com>
2019-04-04 15:40:45 +0100

    style buttons

--- public/script.js
+++ public/script.js
@@ -10,8 +10,8 @@ const displayStationData = data => {
   populateDom(data);
 };
 
-window.onload = defaultStation();
-window.onload = setInterval(getTime, 1000);
+defaultStation();
+setInterval(getTime, 1000);
 
 const button = document.querySelector("#submit-button");
 const input = document.querySelector("#user-input");
@@ -32,40 +32,3 @@ button.addEventListener("click", e => {
     .then(res => res.json())
     .then(json => displayStationData(json));
 });
-
-const testObject = {
-  inbound: [
-    {
-      lineName: "victoria",
-      destinationName: "seven sisters",
-      timeToStation: 67
-    },
-    {
-      lineName: "victoria",
-      destinationName: "sevsdsters",
-      timeToStation: 637
-    },
-    {
-      lineName: "victoria",
-      destinationName: "sdfushers",
-      timeToStation: 6456
-    }
-  ],
-  outbound: [
-    {
-      lineName: "piccadilly",
-      destinationName: "askuhdas",
-      timeToStation: 63
-    },
-    {
-      lineName: "piccadilly",
-      destinationName: "ss",
-      timeToStation: 567
-    },
-    {
-      lineName: "piccadilly",
-      destinationName: "syyyeeee",
-      timeToStation: 635
-    }
-  ]
-};
